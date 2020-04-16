734ac86bae3b2363f073a23f95e4c14cb360206d
rui <github@rui.fr>
2020-04-16 14:47:07 +0200

    final working version without setting of options

--- components/agile-voting/agile-voting.js
+++ components/agile-voting/agile-voting.js
@@ -12,7 +12,7 @@
     <style>
         :host {
             position:relative;
-        display:flex;
+            display:flex;
             justify-content : center;
             align-items : center;
             width : 200px;
@@ -21,10 +21,10 @@
             color : white;
             border-radius : 5px;
         }
-        .value {
+        #value {
             font-size: 4em;
         }
-        .reset {
+        #reset {
             font-size:1.5em;
             display:block;
             position:absolute;
@@ -32,11 +32,68 @@
             right:5px;
             z-index:50;
         }
+        #form {
+            background-color : white;
+            width:100%;
+            height:100%;
+            display:flex;
+            flex-direction:column;
+            justify-content : center;
+            align-items : center;
+        }
+        #selector {
+            display: block;
+            font-size: 2em;
+            font-weight: 700;
+            color: #444;
+            line-height: 2em;
+            outline:none;
+            box-sizing: border-box;
+            width: 90%;
+            max-width: 90%;
+            margin: 0;
+            padding:1px 10px 1px 10px;
+            border: 1px solid #aaa;
+            box-shadow: 0 1px 0 1px rgba(0,0,0,.04);
+            border-radius: .5em;
+            -moz-appearance: none;
+            -webkit-appearance: none;
+            appearance: none;
+            background-color: #fff;
+            background-image: url('data:image/svg+xml;charset=US-ASCII,%3Csvg%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20width%3D%22292.4%22%20height%3D%22292.4%22%3E%3Cpath%20fill%3D%22%23007CB2%22%20d%3D%22M287%2069.4a17.6%2017.6%200%200%200-13-5.4H18.4c-5%200-9.3%201.8-12.9%205.4A17.6%2017.6%200%200%200%200%2082.2c0%205%201.8%209.3%205.4%2012.9l128%20127.9c3.6%203.6%207.8%205.4%2012.8%205.4s9.2-1.8%2012.8-5.4L287%2095c3.5-3.5%205.4-7.8%205.4-12.8%200-5-1.9-9.2-5.5-12.8z%22%2F%3E%3C%2Fsvg%3E'),
+                linear-gradient(to bottom, #ffffff 0%,#e5e5e5 100%);
+            background-repeat: no-repeat, repeat;
+            background-position: right .7em top 50%, 0 0;
+            background-size: 0.65em auto, 100%;
+        } 
+
+        #setValue {
+            width:90%;
+            border-radius : 10px;
+            background-color:#EEE;
+            box-sizing: border-box;
+            text-align:center;
+                    transition: all 0.2s;
+            font-size: 2em;
+            font-weight: 700;
+            color: #444;
+            line-height: 2em;
+            outline:none;
+            
+        }
+        #setValue:hover {
+            background-color:#4eb5f1;
+        }
     </style>
-    
-    <div id="main">
-        <span class="value">XL</span>
-        <span class="reset">🔄</span>
+    <div id="form">
+        <select id="selector">
+           
+        </select>
+        <button id="setValue">vote!</button>
+    </div>
+    <div id="info">
+        <span id="value"></span>
+        <span id="reset">🔄</span>
     <div>`;
 
   //define the component class from HTMLElement inheritance
@@ -47,27 +104,94 @@
             //attach template to shadow root
             this.attachShadow({mode:'open'});
             this.shadowRoot.appendChild(template.content.cloneNode(true));
+            //bind functions to instance:
+            this.render = this.render.bind(this);
+            this.resetValue = this.resetValue.bind(this);
+            this.setValueFromCombo = this.setValueFromCombo.bind(this);
+            this.ensureFormShownIfNoValueSet = this.ensureFormShownIfNoValueSet.bind(this);
+            //map elements to component variables for easier manipulation:
+            this.$valueInfo = this.shadowRoot.querySelector('#value');
+            this.$formView = this.shadowRoot.querySelector('#form');
+            this.$selector = this.shadowRoot.querySelector('#selector');
+            this.$buttonSet = this.shadowRoot.querySelector('#setValue');
+            this.$infoView = this.shadowRoot.querySelector('#info');
+            this.$buttonReset = this.shadowRoot.querySelector('#reset');
             log('AgileVoting component instantiated')
         }
 
-connectedCallback(){
-    log('AgileVoting component loaded into the page')
-    if(!this.hasAttribute('value')){
-        this.setAttribute('value','');
-    }
-}
-get value() {
-    return this.getAttribute('value');
-}
-set value(newValue) {
-    this.setAttribute('value', newValue);
-}
+        connectedCallback(){
+            log('AgileVoting component loaded into the page')
+            if(!this.hasAttribute('value')){
+                this.setAttribute('value','');
+            }
+            this.listOfValues = ['XS','S','M','L','XL'];
+            this.$buttonReset.addEventListener('click',this.resetValue)
+            this.$buttonSet.addEventListener('click',this.setValueFromCombo);
+            this.render();
+        }
+        resetValue()
+        {
+            this.value = '';
+        }
+        setValueFromCombo()
+        {
+            this.value = this.$selector.value;
+        }
+        static get observedAttributes() {
+            return ['value'];
+        }
+        ensureFormShownIfNoValueSet()
+        {
+            if(this.value != '')
+            {
+                this.$infoView.style.display = 'flex';
+                this.$formView.style.display = 'none';
+            } else {
+                this.$infoView.style.display = 'none';
+                this.$formView.style.display = 'flex';
+            }
+        }
+        get value() {
+            return this.getAttribute('value') || '';
+        }
+        set value(newValue) {
+            this.setAttribute('value', newValue);
+            this.render();
+            var event = new CustomEvent('valueChanged',{'detail':newValue});
+            this.dispatchEvent(event);
+        }
+
+        set listOfValues(values)
+        {
+            this.DefineOptions(values);
+        }
+
+        DefineOptions(values)
+        {
+            for(var i=0;i<this.$selector.options.length;i++)
+            {
+                this.$selector.remove(i);
+            }
+
+            for(var i=0;i<values.length;i++)
+            {
+                const option = document.createElement('option');
+                option.value = values[i];
+                option.text = values[i];
+                this.$selector.add(option);
+            }
+        }
+
+        render()
+        {
+            this.ensureFormShownIfNoValueSet();
+            this.$valueInfo.innerHTML = this.value;
+        }
     }
     //register the component into the customElements list of the page
     customElements.define('rc-agile-voting',AgileVoting);
     log('rc-agile-voting defined in custom elements');
 
-    
     //export the component in order to be able to use it programmatically in js
     module.exports = AgileVoting; 
 })();
--- components/agile-voting/agile-voting.test.js
+++ components/agile-voting/agile-voting.test.js
@@ -13,6 +13,19 @@ const select = (comp,name) => comp.shadowRoot.querySelector(name);
 //start our test suite
 describe('<rc-agile-voting/>', () => {
 
+    //helper to expose once for all the inner elements of the component
+    function extractElements(componentName){
+        const component = document.querySelector(componentName);
+        return {
+          component,
+          form : select(component,'#form'),
+          info : select(component,'#info'),
+          value : select(component,'#value'),
+          selector : select(component,'#selector'),
+          setvalue : select(component,'#setValue'),
+          reset : select(component, '#reset')
+        };
+    }
     //setup the test suite:
     //before each test run fill the current test document with the content
     //of our test document
@@ -20,18 +33,116 @@ describe('<rc-agile-voting/>', () => {
         document.documentElement.innerHTML = html.toString();
     });
    
-    //start our scenario:
+    //scenario with a component with a valid value
     describe('given I have a component with valid value', () => {
         //first try to find the component in the document
         it('should find my component', () => {
-            const component = document.querySelector('#with-value-set');
+            const {component} = extractElements('#with-value-set');
             expect(component).not.toBeNull();
         })
 
         it('should have value set', () => {
-            const component = document.querySelector('#with-value-set');
-            expect(component.value).toEqual('XL');
-        })
+            const {component} = extractElements('#with-value-set');
+            expect(component.value).toEqual('XS');
+        });
 
+        it('should show initial value set in html', () => {
+            const {value} = extractElements('#with-value-set');
+            expect(value.innerHTML).toEqual('XS');
+        });
+
+        it('should show value set programmatically', () => {
+            const {component,value} = extractElements('#with-value-set');
+            component.value = 'M';
+            expect(value.innerHTML).toEqual('M');
+        });
+
+        it('should reset value when icon clicked', () => {
+            const {component,reset} = extractElements('#with-value-set');
+            reset.click();
+            expect(component.value).toEqual('');
+        });
+
+        it('should switch views when icon clicked', () => {
+            const {info,form,reset} = extractElements('#with-value-set');
+            reset.click();
+            expect(form.style.display).not.toEqual('none');
+            expect(info.style.display).toEqual('none');    
+        });
     });
-});
+
+    describe('given I have a component without valid value', () => {
+
+        it('should show form view when no value set', () => {
+            const {component,info,form} = extractElements('#with-value-not-set');
+            expect(component.value).toEqual('');
+            expect(form.style.display).not.toEqual('none');
+            expect(info.style.display).toEqual('none'); 
+            
+        })
+        it('should show elements to select value',() => {
+            const {selector,reset} = extractElements('#with-value-not-set');
+            expect(selector).not.toBeNull();  
+            expect(reset).not.toBeNull();   
+        });
+
+        describe('when I select a value',() => {
+            it('it sets the value on the component',() => {
+                const {component,selector,setvalue} = extractElements('#with-value-not-set');
+                selector.value = 'M';
+                setvalue.click();
+                expect(component.value).toEqual('M');
+            })
+
+            it('it shows the info view',() => {
+                const {selector,info,form,setvalue} = extractElements('#with-value-not-set');
+                selector.value = 'M';
+                setvalue.click();
+                expect(form.style.display).toEqual('none');
+                expect(info.style.display).not.toEqual('none');
+            });
+
+            it('throws an event "valueChanged"', () => {
+                const {component,selector,setvalue} = extractElements('#with-value-not-set');
+                var valueFromEvent = "";
+                component.addEventListener('valueChanged',(e) => {
+                    valueFromEvent = e.detail;
+                });
+                selector.value = 'M';
+                setvalue.click();
+                expect(valueFromEvent).toEqual('M');
+            });
+
+        })
+    });
+
+    describe('I need to integrate my component in something else', () => {
+
+        it('should create programmaticaly my component',()=> {
+            const component = new AgileVoting();
+            var valueFromEvent = "";
+            component.addEventListener('valueChanged',(e) => {
+                valueFromEvent = e.detail;
+            });
+            expect(component.value).toEqual('');
+            component.value = 'XL';
+            expect(valueFromEvent).toEqual('XL');
+        });
+
+        it('should set the possible values programmaticaly', () => {
+            const component = new AgileVoting();
+            const selector = select(component,'#selector');
+            component.DefineOptions(['small','medium','big']);
+            var valueFromEvent = "";
+            component.addEventListener('valueChanged',(e) => {
+                valueFromEvent = e.detail;
+            });
+            component.value = selector.options[1].value;
+            expect(valueFromEvent).toEqual('medium'); 
+        })
+    });
+}); 
+
+
+
+
